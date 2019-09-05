f2264f2483d60d9785b738befefda11d2e0dbd05
Jaden Antoine <jadenjantoine@gmail.com>
2019-09-05 19:06:03 -0400

    Update StraightLine.h

--- StraightLine.h
+++ StraightLine.h
@@ -1,29 +1,30 @@
-class StraightLine{
+class StraightLine {
   private:
-  
+
     double slope, xintercept, yintercept;
 
   public:
     StraightLine(); //non paramaterized constructor
-    
+
     StraightLine(double k, double b);// paramaterized constructor
-    
+
     void expressionofLine(); //returns expression of line in y = k * x + b form
-    
+
     double getSlope() const;// returns the slope
-    
-    double getXintercept() const; // returns the x intercept
-    
+
+    double getXintercept(); // returns the x intercept
+
     double getYintercept() const; // returns the y intercept
-   
-    void horizontalShift(); // shifts the line to the left or right, by changing the x intercept
-    
-    void verticalShift();//shifts the line up or down using the y intercept
-   
+
+    void horizontalShift(double shiftBy); // shifts the line to the left or right, by changing the x intercept
+
+    void verticalShift(double shiftBy);//shifts the line up or down using the y intercept
+
     void rotateLine(); // rotates the line by changing the slope
-    
+
     bool operator ||(const StraightLine& L2) const; // returns whether or not the line is parallel to another (L1 || L2)
-    
+
     void operator *(int m); // multiplies the line by an integer number ( Line1 * m)
-    
-}
+
+};
+
