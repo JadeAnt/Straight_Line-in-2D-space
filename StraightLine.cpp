f3a77e6e17fce74c38fca7e91c53a41a116a1d48
Jaden Antoine <jadenjantoine@gmail.com>
2019-09-05 16:43:45 -0400

    Update StraightLine.cpp

--- StraightLine.cpp
+++ StraightLine.cpp
@@ -7,13 +7,16 @@ using namespace std;
 StraightLine(){ //non paramaterized constructor
 
   cout << "Constructing Straight Line Object." << endl;
-
+  slope = 0;
+  yintercept = 0;
+  
 }
 
 StraightLine::StraightLine(double k, int b){ // paramaterized constructor
   cout << "Constructing Straight Line Object." << endl;
   slope = k;
   yintercept = b;
+  
 }
 
 void StraightLine::expressionofLine(){ //returns expression of line in y = k * x + b form
@@ -30,26 +33,28 @@ double StraightLine::getSlope() const{ // returns the slope
 
 double StraightLine::getXintercept() const{ // returns the x intercept
   
-  xintercept = (-yintercept) / (slope) // 0 = k * x + b -> -b/k = x
-  return xintercept;
+  xintercept = (-yintercept) / (slope); // 06 = k * x + b -> -b/k = x
+  return xintercept; // in coordinate form??
   
 }
 
 double StraightLine::getYintercept() const{ // returns the y intercept
 
-  return yintercept;
+  return yintercept; // In coordinate form??
 
 }
 
-void StraightLine::horizontalShift(double shiftBy){
+void StraightLine::horizontalShift(double shiftBy){ //shifts the line left or right depending on the parameter
   xintercept += shiftBy;
+  cout << "New X intercept = " << xintercept << endl;
 }
     
-void StraightLine::verticalShift(double shiftBy){
+void StraightLine::verticalShift(double shiftBy){ //shifts line up or down depending on the parameter
   yintercept += shiftBy;
+  cout << "New Y intercept = " << yintercept << endl;
 }
    
-void StraightLine::rotateLine();
+void StraightLine::rotateLine(); //rotates the line 90 degrees by changing the slope
     
 bool StraightLine::operator ||(const StraightLine& L2) const{ // checks if the two lines are parallel (L1 || L2)
 
